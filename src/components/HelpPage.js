65f49d52627ebbb2e86e9169c1b7938bb8f8be31
nathanielpascual <nath_pascual@yahoo.com>
2018-11-21 19:30:44 +0800

    created smaller components

---
+++ src/components/HelpPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const HelpPage = () => (
+    <div>
+        This is my help component.
+    </div>
+);
+
+export default HelpPage;
---
+++ src/components/NotFoundPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const NotFoundPage = () => (
+    <div>
+        Error 404! - <Link to="/">Go Home</Link>
+    </div>
+);
+
+export default NotFoundPage;
