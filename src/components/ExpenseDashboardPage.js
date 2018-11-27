b43d2d9d9a465763427d3b10a318887736365885
nathanielpascual <nath_pascual@yahoo.com>
2018-11-27 23:15:47 +0800

    Mocking libraries with jest and testing user interaction

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
 import ExpenseListFilter from './ExpenseListFilter'
+
 const ExpenseDashboardPage = () => (
     <div>
         <ExpenseListFilter/>
