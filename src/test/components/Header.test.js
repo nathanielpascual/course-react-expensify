31bff17ef00f1194a25817dd5b3b3e63174b7ebd
nathanielpascual <nath_pascual@yahoo.com>
2018-12-04 04:57:20 +0800

    added header with error test

--- src/test/components/Header.test.js
+++ src/test/components/Header.test.js
@@ -4,6 +4,15 @@ import toJSON from 'enzyme-to-json';
 import { Header } from '../../components/Header';
 
 test('should render Header correctly',()=>{
-    const wrapper = shallow(<Header signOut={()=>{ }}/>);
+    const wrapper = shallow(<Header startLogout={()=>{ }}/>);
     expect(toJSON(wrapper)).toMatchSnapshot();
 });
+
+test('should call logout on button click', () => {
+  const startLogout = jest.fn();
+  const wrapper = shallow(<Header startLogout={startLogout} />);
+  wrapper.find('button').simulate('click');
+  expect(startLogout).toHaveBeenCalled();
+});
+
+
--- src/test/components/LoginPage.test.js
+++ src/test/components/LoginPage.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import toJSON from 'enzyme-to-json';
-import LoginPage from '../../components/LoginPage';
+import {LoginPage} from '../../components/LoginPage';
 
 test('should render LoginPage ',()=>{
     const wrapper = shallow(<LoginPage startLogin={()=>{}}/>);
     expect(toJSON(wrapper)).toMatchSnapshot();
+});
+
+test('should call login on button click',()=>{
+    // const startLogin = jest.fn();//spies
+    // const wrapper = shallow(<Login startLogin={startLogin}/>);
+    // wrapper.find('button').simulate('click');
+    // expect(startLogin).toHaveBeenCalled();
+
 });
