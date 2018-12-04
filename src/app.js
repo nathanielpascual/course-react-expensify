6bcb9cae34284e4d1bfc9304028e82bb92e14e98
nathanielpascual <nath_pascual@yahoo.com>
2018-12-04 13:03:25 +0800

    add auth test in action and reducers

--- src/app.js
+++ src/app.js
@@ -2,13 +2,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
+import {firebase} from './firebase/firebase';
 import {startSetExpenses} from './actions/expenses';
 import AppRouter, { history }  from './routers/AppRouters';
 import configureStore from './stores/configureStore';
+import {login,logout} from './actions/auth';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 import 'react-dates/lib/css/_datepicker.css';
-import {firebase} from './firebase/firebase';
+
 
 const store = configureStore();
 
@@ -33,7 +35,7 @@ const renderApp = ()=> {
 
 firebase.auth().onAuthStateChanged((user)=>{
  if(user){
-   
+    store.dispatch(login(user.uid));
     store.dispatch(startSetExpenses()).then(()=>{
         renderApp();
 
@@ -43,6 +45,7 @@ firebase.auth().onAuthStateChanged((user)=>{
     });
  }
  else{
+     store.dispatch(logout());
     renderApp();
     history.push('/'); 
 }
