c9a2ea4af213d07aaafe8bddeae45bca3c77b877
nathanielpascual <nath_pascual@yahoo.com>
2018-12-05 22:49:45 +0800

    Styling inputs and Expense forms

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -63,23 +63,24 @@ export default class ExpenseForm extends React.Component {
 
     render(){
         return (
-            <div>
-                {this.state.error && <p>{this.state.error}</p>}
-                <form onSubmit={this.onSubmit}>
+                <form className="form" onSubmit={this.onSubmit}>
+                   {this.state.error && <p className="form__error">{this.state.error}</p>}
                     <input 
                         type="text"
+                        className="text-input"
                         placeholder="Description"
                         autoFocus
                         value={this.state.description}
                         onChange={this.onDescriptionChange}/>
-                    <br/>
+
                     <input
                         type="text"
+                        className="text-input"
                         placeholder="Amount"
                         value = {this.state.amount}
                         onChange ={this.onAmountChange}
                     />
-                    <br/>
+
                     <SingleDatePicker
                         date = {this.state.createdAt}
                         onDateChange = {this.onDateChange}
@@ -88,15 +89,18 @@ export default class ExpenseForm extends React.Component {
                         numberOfMonths={1}
                         isOutsideRange = {()=> false }
                     />
-                    <br/>
+
                     <textarea
+                         className="textarea"
                          placeholder ="Add a note for your expense (optional)"
                          value={this.state.note}
                          onChange={this.onNoteChange}/>
-                    <br/>
-                    <button>Save Expense</button>
+ 
+                    <div>
+                       <button className="button">Save Expense</button>
+                    </div>
+                   
                 </form>
-            </div>
         )
     }
 }
--- src/components/ExpenseListFilter.js
+++ src/components/ExpenseListFilter.js
@@ -34,12 +34,15 @@ export class ExpenseListFilter extends React.Component{
             <div className="content-container">
             <div className="input-group">
                 <div className="input-group__item"><input 
+                    className="text-input"
+                    placeholder="Search Expenses"
                     type="text" 
                     value={this.props.filters.text} 
                     onChange={this.onTextChange}/>
                 </div>
                 <div className="input-group__item">
                     <select
+                    className="select"
                     value = {this.props.filters.sortBy} 
                     onChange={this.onSortChange}>
                     <option value="date">Date</option>
